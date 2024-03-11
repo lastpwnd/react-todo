776cf0a1f706093848bb0c307c9f232df7a96fd9
lastpwnd <last.pwnd@gmail.com>
2024-03-11 16:19:57 -0400

    lesson_1_3 final

--- src/AddToDoForm.jsx
+++ src/AddToDoForm.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-const AddToDoForm = () => {
+const AddToDoForm = (props) => {
+
+function handleAddTodo(event) 
+  {
+    event.preventDefault()
+    // Instructions are unclear: either preventDefault() and fixating the inner value or clearing the input and console after default Submit event
+    const toDoTitle = event.target.title.value
+    console.log(toDoTitle)
+    props.addToDoForm(toDoTitle)
+    // alert(`You have entered -> ${toDoTitle} \n Value is logged in console \n Now the form will be cleared` )
+  }
+  
+
   return (
    <div>
-        <form id="toDoForm"> 
+        <form id="toDoForm" onSubmit={handleAddTodo}> 
             <label htmlFor='toDoTitle'> Title </label> 
-            <input id='toDoTitle' />  
+            <input id='toDoTitle' name='title' />  
             <button type='submit'> Add </button>
         </form>
    </div>
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
 import './App.css'
+import { useState } from 'react'
 import ToDoList from './ToDoList'
 import AddToDoForm from './AddToDoForm'
 
 function App() {
+
+  const [newToDo, setNewToDo] = useState("123")
+
+
   return (
     <div>
       <h1> Todo List </h1>
-      <AddToDoForm />
+      <AddToDoForm addToDoForm={setNewToDo} />
+      <p> {newToDo} </p>
       <ul> <ToDoList /> </ul>
     </div>
   )
