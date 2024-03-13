245e9cb3db49e81ba1a244024152bf61db61b304
lastpwnd <last.pwnd@gmail.com>
2024-03-12 20:15:19 -0400

    lesson_1_3 fix

--- src/AddToDoForm.jsx
+++ src/AddToDoForm.jsx
@@ -5,11 +5,10 @@ const AddToDoForm = (props) => {
 function handleAddTodo(event) 
   {
     event.preventDefault()
-    // Instructions are unclear: either preventDefault() and fixating the inner value or clearing the input and console after default Submit event
     const toDoTitle = event.target.title.value
     console.log(toDoTitle)
     props.addToDoForm(toDoTitle)
-    // alert(`You have entered -> ${toDoTitle} \n Value is logged in console \n Now the form will be cleared` )
+    document.getElementById("toDoForm").reset()
   }
   
 
